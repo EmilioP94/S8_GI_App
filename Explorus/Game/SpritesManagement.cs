b7a05574a43a92e1fa90a640dbd205f19630a6e6
Galvadax <Mickael.Calve@Usherbrooke.ca>
2022-09-02 20:39:58 -0400

    Small flyweight factory rework

---
+++ Explorus/Game/SpritesManagement.cs
@@ -0,0 +1,30 @@
+﻿using Explorus.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorus.Game
+{
+    internal static class SpritesManagement
+    {
+        public static Dictionary<Sprites, Rectangle> GetSpritesRegions()
+        {
+            Dictionary<Sprites, Rectangle> spritesRegions = new Dictionary<Sprites, Rectangle>()
+            {
+                { Sprites.wall, new Rectangle(0, 0, Constants.unit * 2, Constants.unit * 2) },
+                { Sprites.title, new Rectangle(Constants.unit * 2, 0, Constants.unit * 4, Constants.unit)},
+                { Sprites.heart, new Rectangle(0, 0, Constants.unit * 2, Constants.unit * 2)},
+                { Sprites.bigBubble, new Rectangle(Constants.unit * 8, 0, Constants.unit, Constants.unit)},
+                { Sprites.smallBubble, new Rectangle(Constants.unit * 9, 0, Constants.unit, Constants.unit)},
+                { Sprites.poppedBubble, new Rectangle(Constants.unit * 10, 0, Constants.unit, Constants.unit)},
+                { Sprites.gem, new Rectangle(Constants.unit * 11, 0, Constants.unit, Constants.unit) },
+                { Sprites.key, new Rectangle(Constants.unit * 12, 0, Constants.unit, Constants.unit) },
+                { Sprites.slimusDownLarge, new Rectangle(0, Constants.unit * 2, Constants.unit * 2, Constants.unit * 2) },
+            };
+            return spritesRegions;
+        }
+    }
+}
---
+++ Explorus/Models/SpritesToImageType.cs
@@ -0,0 +1,38 @@
+﻿using Explorus.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorus.Models
+{
+    internal static class SpritesToImageType
+    {
+        public static ImageType ConvertSpritesToImageType(Sprites sprite)
+        {
+            switch (sprite)
+            {
+                case Sprites.wall:
+                    return ImageType.Wall;
+
+                case Sprites.heart:
+                case Sprites.bigBubble:
+                case Sprites.gem:
+                    return ImageType.Collectible;
+
+                case Sprites.slimusDownSmall:
+                case Sprites.slimusDownMedium:
+                case Sprites.slimusDownLarge:
+                    return ImageType.Player;
+
+                case Sprites.poppedBubble:
+                case Sprites.smallBubble:
+                case Sprites.title:
+                case Sprites.key:
+                default:
+                    return ImageType.Other;
+            }
+        }
+    }
+}
