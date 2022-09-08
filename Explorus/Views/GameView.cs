cddf232c9a9a03b883e8e68f0b4d404733f830e9
jeff <jeff_miller16@hotmail.com>
2022-09-08 09:55:56 -0400

    scaling works still needs padding

--- Explorus/Views/GameView.cs
+++ Explorus/Views/GameView.cs
@@ -39,13 +39,12 @@ namespace Explorus
         public GameView(HandleInput doHandle, ILabyrinth lab)
         {
             //add header in calculation ( its 2 units )
-            this.originalSmallestSide = Math.Min(Constants.LabyrinthHeight, Constants.LabyrinthHeight) * Constants.unit;
+            this.originalSmallestSide = Math.Min(Constants.LabyrinthHeight, Constants.LabyrinthHeight) * Constants.unit * 2;
             oGameForm = new GameForm();
-            //math.min heigth vs width
-            this.scaleFactor = this.computeScaleFactor(originalSmallestSide, oGameForm.ClientSize.Width);
+            this.scaleFactor = this.computeScaleFactor(originalSmallestSide, Math.Min(oGameForm.ClientSize.Width,oGameForm.ClientSize.Height));
             oGameForm.Paint += new PaintEventHandler(this.GameRenderer);
             oGameForm.KeyDown += new KeyEventHandler(doHandle);
-            oGameForm.ResizeEnd += new EventHandler(ProcessResize);
+            oGameForm.Resize += new EventHandler(ProcessResize);
             labyrinthView = new LabyrinthView(lab);
         }
 
@@ -72,22 +71,33 @@ namespace Explorus
         {
             Graphics g = e.Graphics;
             g.Clear(Color.Black);
-            g.ScaleTransform(1.0F,1.0F);//((float)this.scaleFactor,(float)this.scaleFactor);
+            g.ScaleTransform((float)this.scaleFactor,(float)this.scaleFactor);
             labyrinthView.Render(sender, e);
             oGameForm.Text = String.Format("Explorus - FPS {0}", framerate.ToString());
         }
 
         private void ProcessResize(object sender, EventArgs e)
         {
-            int smallestWindowSide = Math.Min(oGameForm.ClientSize.Width, oGameForm.ClientSize.Height);
-            double newScaleFactor = this.computeScaleFactor(this.originalSmallestSide, (double)smallestWindowSide);
-            this.scaleFactor = newScaleFactor;
+            if(oGameForm.ClientSize.Width >= oGameForm.ClientSize.Height)
+            {
+                double newScaleFactor = this.computeScaleFactor(this.originalSmallestSide, (double)oGameForm.ClientSize.Height);
+                this.scaleFactor = newScaleFactor;
+                int horizontalPadding = (oGameForm.ClientSize.Width - oGameForm.ClientSize.Height)/2;
+                //oGameForm.Padding = new System.Windows.Forms.Padding(horizontalPadding,0,horizontalPadding,0);
+            }
+            if(oGameForm.ClientSize.Height >= oGameForm.ClientSize.Width)
+            {
+                double newScaleFactor = this.computeScaleFactor(this.originalSmallestSide, (double)oGameForm.ClientSize.Width);
+                this.scaleFactor = newScaleFactor;
+                int verticalPadding = (oGameForm.ClientSize.Height - oGameForm.ClientSize.Width)/2;
+                //oGameForm.Padding = new System.Windows.Forms.Padding(0,verticalPadding,0,verticalPadding);
+            }
         }
 
         private double computeScaleFactor(double originalSideLength, double newSideLength)
         {
             double newScaleFactordelta = newSideLength / originalSideLength;
-            return 1 + (Math.Round(newScaleFactordelta, 2)/10);
+            return Math.Round(newScaleFactordelta, 2);
 
         }
     }
