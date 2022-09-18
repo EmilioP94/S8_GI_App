43217f952148e16b6911b4fa23f56d04d9e5e8d0
Emile Paquette <paqe3001@usherbrooke.ca>
2022-09-18 14:07:59 -0400

    remove comments in pauseview.

--- Explorus/Views/PauseView.cs
+++ Explorus/Views/PauseView.cs
@@ -19,32 +19,16 @@ namespace Explorus.Views
             this.height = height;
         }
 
-        //Rectangle rect;
-
-        //public PauseView(Rectangle rect)
-        //{
-        //    this.rect = rect;
-        //}
-
         public void Render(object sender, PaintEventArgs e, Point offset)
         {
-            //rect.X = rect.X + offset.X;
-            //rect.Y = rect.Y + offset.Y;
             Rectangle rect = new Rectangle(offset.X, offset.Y, width, height);
             StringFormat stringFormat = new StringFormat();
             stringFormat.Alignment = StringAlignment.Center;      // Horizontal Alignment
             stringFormat.LineAlignment = StringAlignment.Center;  // Vertical Alignment
             e.Graphics.DrawString("Paused", new Font("Times New Roman", 32), Brushes.White, rect, stringFormat);
-            //e.Graphics.DrawRectangle(Pens.White, rect);
             SolidBrush brush = new SolidBrush(Color.FromArgb(75, Color.Black));
             e.Graphics.FillRectangle(brush, rect);
         }
 
-        //public void Resize(int width, int height)
-        //{
-        //    //Console.WriteLine($"width: {width}, height: {height}");
-        //    this.width = width;
-        //    this.height = height;
-        //}
     }
 }
