6e0b13b5634191e1cd076a54a50e354c85fcd0a2
Mathieu Gaudet <math.gaudet195@gmail.com>
2022-09-22 17:05:56 -0400

    Audio Thread test

---
+++ TestExplorus/Threads/AudioThreadTest.cs
@@ -0,0 +1,86 @@
+﻿using Explorus.Controllers;
+using Explorus.Threads;
+using Explorus.Views;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestExplorus.Threads
+{
+    [TestClass]
+    public class AudioThreadTest
+    {
+        [TestMethod]
+        public void TestGetAudioThread()
+        {
+            AudioThread audioThread = AudioThread.GetInstance();
+            Assert.IsTrue(audioThread != null);
+
+        }
+
+        [TestMethod]
+        public void TestMusicVolumeIncrement()
+        {
+            AudioThread audioThread = AudioThread.GetInstance();
+            Assert.IsTrue(audioThread != null);
+
+            double volume = audioThread.musicVolume;
+            audioThread.QueueEvent(SoundsEvents.IncrementMusic);
+
+            Thread.Sleep(100);
+            Assert.AreEqual(audioThread.musicVolume, volume + 0.01);
+        }
+        [TestMethod]
+        public void TestMusicVolumeDecrement()
+        {
+            AudioThread audioThread = AudioThread.GetInstance();
+            Assert.IsTrue(audioThread != null);
+
+            double volume = audioThread.musicVolume;
+            audioThread.QueueEvent(SoundsEvents.DecrementMusic);
+
+            Thread.Sleep(100);
+            Assert.AreEqual(audioThread.musicVolume, volume - 0.01);
+        }
+
+        [TestMethod]
+        public void TestSoundVolumeIncrement()
+        {
+            AudioThread audioThread = AudioThread.GetInstance();
+            Assert.IsTrue(audioThread != null);
+
+            double volume = audioThread.soundVolume;
+            audioThread.QueueEvent(SoundsEvents.IncrementSounds);
+
+            Thread.Sleep(100);
+            Assert.AreEqual(audioThread.soundVolume, volume + 0.01);
+        }
+        [TestMethod]
+        public void TestSoundVolumeDecrement()
+        {
+            AudioThread audioThread = AudioThread.GetInstance();
+            Assert.IsTrue(audioThread != null);
+
+            double volume = audioThread.soundVolume;
+            audioThread.QueueEvent(SoundsEvents.DecrementSounds);
+
+            Thread.Sleep(100);
+            Assert.AreEqual(audioThread.soundVolume, volume - 0.01);
+        }
+
+        [TestMethod]
+        public void TestMusicPause()
+        {
+            AudioThread audioThread = AudioThread.GetInstance();
+            Assert.IsTrue(audioThread != null);
+
+            audioThread.StopMusic();
+            audioThread.ResumeMusic();
+        }
+    }
+}
