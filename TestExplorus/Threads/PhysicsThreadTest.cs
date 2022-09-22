4076e78185ae96756ae79f28ef18bc6d4d620ff5
Galvadax <Mickael.Calve@Usherbrooke.ca>
2022-09-22 14:43:54 -0400

    empty thread test folder

---
+++ TestExplorus/Threads/PhysicsThreadTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestExplorus.Threads
+{
+    [TestClass]
+    public class PhysicsThreadTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
